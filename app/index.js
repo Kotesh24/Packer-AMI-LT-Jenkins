53a4c1bcc6b4df5d1152807940799cf07153cf6e
Koteshwararao Banothu <104509034+Kotesh24@users.noreply.github.com>
2026-02-26 08:16:14 +0000

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js app running on custom AMI! by using packer workshop deployed successfully thank you packer');
+  res.send('Hello from Node.js app running on custom AMI! by using packer workshop deployed successfully thank you packer Build Once Deploy Forever Immutable AMI Deployment with Packer Deploy into live Environment without Rollback');
 });
 // veera
 
